c2582ac7522df0d421d25d7a8114d86bf4eaba97
speedypleath <gheorgheandrei13@gmail.com>
2022-08-17 07:46:46 +0300

    relink cli

--- src/midi_generator/__init__.py
+++ src/midi_generator/__init__.py
@@ -3,4 +3,4 @@ from midi_generator.config import *
 from midi_generator.genetic import *
 from midi_generator.output import *
 from midi_generator.syncopation import *
-from cli import print_cli
+from midi_generator.cli import print_cli
