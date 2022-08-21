4e8c151848b7ed0b2b0567fee3d27702fa39f85f
speedypleath <gheorgheandrei13@gmail.com>
2022-08-21 14:25:01 +0300

    automation

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ extensions = Extension('note',
                 include_dirs=['/opt/homebrew/include'],
                 library_dirs=['/opt/homebrew/Cellar/boost-python3/1.79.0_1/lib'],
                 runtime_library_dirs=['/opt/homebrew/Cellar/boost-python3/1.79.0_1/lib'],
-                libraries=['boost_python3'])
+                libraries=['boost_python'])
 
 setup(ext_modules=[extensions])
