702d829ce773d9ff0f9cd2b0085fbc2672a3c751
speedypleath <gheorgheandrei13@gmail.com>
2022-09-01 00:27:01 +0300

    pybind

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 from setuptools.extension import Extension
+import sys
+
+if sys.platform == 'darwin':
+    from distutils import sysconfig
+    vars = sysconfig.get_config_vars()
+    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
 extensions = Extension('note',
                        sources=['src/lib/midi_generator/build.cpp'],
-                       include_dirs=['/usr/include'],
-                       library_dirs=['/usr/lib'],
-                       runtime_library_dirs=['/usr/lib'],
-                       libraries=[':libboost_python310.so'])
+                       include_dirs=['/opt/homebrew/include'],
+                       library_dirs=['/opt/homebrew/lib/lib'],
+                       runtime_library_dirs=['/opt/homebrew/lib'],
+                       libraries=['boost_python310'])
 
 setup(ext_modules=[extensions])
--- src/midi_generator/cli/__main__.py
+++ src/midi_generator/cli/__main__.py
@@ -10,13 +10,14 @@ from ..utils import powerset, NOTE_DURATIONS
 from ..config import Configuration, parse_config_for_generate
 from .commands import generate, mutate, continue_sequence
 
+
 class CustomParser(argparse.ArgumentDefaultsHelpFormatter):
     def __init__(self, *args, **kwargs):
         super().__init__(
-                 kwargs.get('prog'),
-                 indent_increment=2,
-                 max_help_position=100,
-                 width=None)
+            kwargs.get('prog'),
+            indent_increment=2,
+            max_help_position=100,
+            width=None)
 
     def _format_action(self, action):
         result = super()._format_action(action)
@@ -45,92 +46,92 @@ class CustomParser(argparse.ArgumentDefaultsHelpFormatter):
 
 
 def print_cli():
-    parser = argparse.ArgumentParser(prog="midi-generator", 
-                            description='Command line interface for b2bAI.', 
-                            usage='%(prog)s [OPTIONS] COMMAND', 
-                            epilog="Run 'midi-generator COMMAND --help' for more information on a command.",
-                            formatter_class=CustomParser)
-    
-    parser.add_argument('-l', '--logging-level', 
-                    choices=[logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL],
-                    type=int,
-                    help='set logging level',
-                    default=logging.INFO)
+    parser = argparse.ArgumentParser(prog="midi-generator",
+                                     description='Command line interface for b2bAI.',
+                                     usage='%(prog)s [OPTIONS] COMMAND',
+                                     epilog="Run 'midi-generator COMMAND --help' for more information on a command.",
+                                     formatter_class=CustomParser)
+
+    parser.add_argument('-l', '--logging-level',
+                        choices=[logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL],
+                        type=int,
+                        help='set logging level',
+                        default=logging.INFO)
 
     subparsers = parser.add_subparsers(title='commands', dest='command')
 
-    generate_seq = subparsers.add_parser('generate', 
-                                description='Generate a MIDI file from a given configuration.',
-                                help='generate a MIDI file',
-                                usage='%(prog)s [OPTIONS] generate {FILE|OPTIONS}', 
-                                formatter_class=CustomParser)
+    generate_seq = subparsers.add_parser('generate',
+                                         description='Generate a MIDI file from a given configuration.',
+                                         help='generate a MIDI file',
+                                         usage='%(prog)s [OPTIONS] generate {FILE|OPTIONS}',
+                                         formatter_class=CustomParser)
 
-    generate_seq.add_argument('-c', '--config', 
-                    type=argparse.FileType('r'),
-                    help='the configuration file', 
-                    metavar='File')
+    generate_seq.add_argument('-c', '--config',
+                              type=argparse.FileType('r'),
+                              help='the configuration file',
+                              metavar='File')
 
-    generate_seq.add_argument('--bars', 
-                    type=int, 
-                    metavar='{4|8}',
-                    help='the number of bars to generate', 
-                    choices=[4, 8], 
-                    default=4)
+    generate_seq.add_argument('--bars',
+                              type=int,
+                              metavar='{4|8}',
+                              help='the number of bars to generate',
+                              choices=[4, 8],
+                              default=4)
 
     generate_seq.add_argument('--rates',
-                    type=set[float], 
-                    metavar='set[float]',
-                    help='the note lengths used to generate the MIDI file', 
-                    default=set(DEFAULT_RATES))
+                              type=set[float],
+                              metavar='set[float]',
+                              help='the note lengths used to generate the MIDI file',
+                              default=set(DEFAULT_RATES))
 
     generate_seq.add_argument('--scale',
-                    type=str,
-                    metavar='Scale',
-                    help='the scale in which to generate the MIDI file',
-                    default='C minor')
+                              type=str,
+                              metavar='Scale',
+                              help='the scale in which to generate the MIDI file',
+                              default='C minor')
 
     generate_seq.add_argument('--density',
-                    type=float,
-                    metavar='float',
-                    help='the density of notes in the generated MIDI file',
-                    default=0.8)
+                              type=float,
+                              metavar='float',
+                              help='the density of notes in the generated MIDI file',
+                              default=0.8)
 
     generate_seq.add_argument('--syncopation',
-                    type=float,
-                    metavar='float',
-                    help='the level of syncopation in the generated MIDI file',
-                    default=0.1)
+                              type=float,
+                              metavar='float',
+                              help='the level of syncopation in the generated MIDI file',
+                              default=0.1)
 
-    generate_seq.add_argument('--ispolyphonic', 
-                    action='store_true',
-                    help='whether to generate a polyphonic or monophonic MIDI file',
-                    default=False)
-                    
-    mutate_seq = subparsers.add_parser('mutate', 
-                            description='Mutate a given MIDI file.',
-                            usage='%(prog)s [OPTIONS] generate FILE [OPTIONS]', 
-                            help='mutate a MIDI file.',
-                            formatter_class=CustomParser)
+    generate_seq.add_argument('--ispolyphonic',
+                              action='store_true',
+                              help='whether to generate a polyphonic or monophonic MIDI file',
+                              default=False)
 
-    mutate_seq.add_argument('file', 
-                    type=str, 
-                    help='file containing sequence to mutate')
+    mutate_seq = subparsers.add_parser('mutate',
+                                       description='Mutate a given MIDI file.',
+                                       usage='%(prog)s [OPTIONS] generate FILE [OPTIONS]',
+                                       help='mutate a MIDI file.',
+                                       formatter_class=CustomParser)
+
+    mutate_seq.add_argument('file',
+                            type=str,
+                            help='file containing sequence to mutate')
 
     mutate_seq.add_argument('--probability',
-                    type=float,
-                    metavar='float',
-                    help='the probability of mutating a note',
-                    default=0.1)
-                    
-    continue_seq = subparsers.add_parser('continue', 
-                            description='Continue a given MIDI sequence.',
-                            usage='%(prog)s [OPTIONS] generate FILE [OPTIONS]', 
-                            help='continue a MIDI sequence.',
-                            formatter_class=CustomParser)
+                            type=float,
+                            metavar='float',
+                            help='the probability of mutating a note',
+                            default=0.1)
 
-    continue_seq.add_argument('file', 
-                    type=argparse.FileType('r'),
-                    help='file containing sequence to continue')
+    continue_seq = subparsers.add_parser('continue',
+                                         description='Continue a given MIDI sequence.',
+                                         usage='%(prog)s [OPTIONS] generate FILE [OPTIONS]',
+                                         help='continue a MIDI sequence.',
+                                         formatter_class=CustomParser)
+
+    continue_seq.add_argument('file',
+                              type=argparse.FileType('r'),
+                              help='file containing sequence to continue')
 
     args = parser.parse_args()
 
@@ -145,7 +146,7 @@ def print_cli():
                 if args.rates not in powerset(NOTE_DURATIONS):
                     logging.warning(' rates not in powerset of note durations, using default rates\n')
                     args.rates = DEFAULT_RATES
-                else: 
+                else:
                     args.rates = list(args.rates)
 
                 key = match_key(args.scale.split(' ')[0].lower())
@@ -171,6 +172,7 @@ def print_cli():
 
         case 'continue':
             return continue_sequence(args.file)
+
 
 if __name__ == '__main__':
     print_cli()
